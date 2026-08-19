1a660769a00980016976ffe508893c01c6edbcd6
Aler <ppland@mail.ru>
2026-08-19 17:17:00 +0300

    Ввод предельного числа

--- first_program.py
+++ first_program.py
@@ -1,5 +1,6 @@
-l = list(range(1, 21))
+n = int(input("Введите число 'n': "))
+l = list(range(1, n + 1))
 print(l)
 
-l1 = [i ** 2 for i in l if i % 2 == 0]
-print(l1)
+l1 = [i**2 for i in l if i % 2 == 0]
+print("Квадраты четных чисел до n: ", l1)
