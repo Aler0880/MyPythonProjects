684cc09b6ca2862dce205c681459785ffea26c51
Aler <ppland@mail.ru>
2026-08-25 12:02:10 +0300

    Учимся получать данные по ключам из json

--- task3 - github_info.py
+++ task3 - github_info.py
@@ -6,6 +6,24 @@ url = "https://api.github.com/repos/python/cpython"
 
 response = requests.get(url)
 
-print(response.status_code)
+data = response.json()
 
-print(response.text[:500])
+name = data.get("name")
+stars = data.get("stargazers_count")
+forks = data.get("forks_count")
+updated = data.get("updated_at")
+nonex = data.get("nonex_key")
+
+print("Название:", name)
+print("Звёзд:", stars)
+print("Форков:", forks)
+print("Обновлён:", updated)
+print("Нет такого: ", nonex)
+
+# print(response.status_code)
+
+# print(response.text[:500])
+
+# print(type(data))
+
+# print(*data.keys(), sep='\n')
