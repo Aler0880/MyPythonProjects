93037810dd7981c43f8c3a270b40135fd559b9a5
Aler <ppland@mail.ru>
2026-08-25 17:05:47 +0300

    Выводим инфу по репе в файл

--- task3 - github_info.py
+++ task3 - github_info.py
@@ -1,24 +1,46 @@
 # Работа с API (requests, JSON)
 
 import requests
+import json
 
-url = "https://api.github.com/repos/python/cpython"
+try:
+    rep = input("Введите название репозитория: ")
+    url = f"https://api.github.com/repos/python/{rep}"  # "https://api.github.com/repos/python/cpython"
 
-response = requests.get(url)
+    response = requests.get(url)
 
-data = response.json()
+    if response.status_code == 404:
+        print("Репозиторий не найден!")
+        with open("result3.txt", "w", encoding="utf-8") as f:
+            f.write("Репозиторий не найден!")
 
-name = data.get("name")
-stars = data.get("stargazers_count")
-forks = data.get("forks_count")
-updated = data.get("updated_at")
-nonex = data.get("nonex_key")
+    # Можно выйти или спросить другой репозиторий
+    else:
+        # try:
+        data = response.json()
+        # except json.JSONDecodeError:
+        # print("Ответ не является JSON")
 
-print("Название:", name)
-print("Звёзд:", stars)
-print("Форков:", forks)
-print("Обновлён:", updated)
-print("Нет такого: ", nonex)
+        name = data.get("name")
+        stars = data.get("stargazers_count")
+        forks = data.get("forks_count")
+        updated = data.get("updated_at")
+        # nonex = data.get("nonex_key")
+
+        print("Название:", name)
+        print("Звёзд:", stars)
+        print("Форков:", forks)
+        print("Обновлён:", updated)
+        with open("result3.txt", "w", encoding="utf-8") as f:
+            f.write(f"Название: {name}\nЗвёзд: {stars}"
+                    f"\nФорков: {forks}\nОбновлён: {updated}")
+        # print("Нет такого: ", nonex)
+
+except requests.exceptions.RequestException:
+
+    print("Ошибка связи")
+    with open("result3.txt", "w", encoding="utf-8") as f:
+        f.write("Ошибка связи")
 
 # print(response.status_code)
 
