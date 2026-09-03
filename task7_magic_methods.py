2719d18379c35240569fb6062c1aa068b8845a66
Aler <ppland@mail.ru>
2026-09-03 15:19:53 +0300

    task7 - продолжение

--- task7_magic_methods.py
+++ task7_magic_methods.py
@@ -21,6 +21,19 @@ class Vector2D:
     def __rmul__(self, other):
         return self.__mul__(other)
 
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __len__(self):
+        return int((self.x**2 + self.y**2) ** 0.5)
+
+    def distance(self, vec1, vec2):
+        vec1 = Vector2D(x1, y1)
+        self.vec1 = Vector2D(v.x, vec1.y)
+        self.vec2 = Vector2D(vec2.x, vec2.y)
+
+        return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
+
 
 v1 = Vector2D(3, 4)
 v2 = Vector2D(5, 7)
@@ -28,9 +41,13 @@ v3 = v1 + v2
 v4 = v2 - v1
 v5 = v1 * 2
 v6 = 2 * v1
+
 print(v1, "+", v2, "=", v3)
 print(v2, "-", v1, "=", v4)
 print(v1, "*", 2, "=", v5)
 print(2, "*", v1, "=", v6)
+print(v1 == v2)
+print(v5 == v6)
+print(len(v1))
 print()
 print(repr(v1), repr(v2), repr(v3), sep="\n")
